e7a2e5f6e2001a1df56e8d7445fcc3c43ca2b323
andre-menezes <andre.menezes@vnda.com.br>
2021-08-31 19:04:46 -0300

    Teste de deploy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = props => {
 
   return (
     <footer>
-      <p>Feito ao som da playlist <a href="https://open.spotify.com/playlist/3Xp13ZRnrVrqVEr3NApJ05?si=V_CYq9PIRQSOmdsWi2SxRg">Letristas</a> por um Dev Jr!</p>
+      <p>Feito ao som da playlist <a href="https://open.spotify.com/playlist/3Xp13ZRnrVrqVEr3NApJ05?si=V_CYq9PIRQSOmdsWi2SxRg">Letristas</a> por um Dev Jr</p>
       <p>Conhece uma música com um belo arranjo de acordes? Então <a href="https://github.com/aaamenezes" target="_blank">me envie a cifra dela</a> e ajude outras milhares de músicos aprimorando a base de músicas do Caosfonia!</p>
     </footer>
   )
